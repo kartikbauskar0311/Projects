dcbea5424697d12ebe60a88195ba6df4bb97dd20
kartik bauskar <kartik93.bauskar@gmail.com>
2024-02-26 19:19:03 -0500

    recaptcha changes

--- Web_to_lead.js
+++ Web_to_lead.js
@@ -6,3 +6,15 @@ function beforesubmit() {
   let formattedDate = new Date(inputDate.value).toLocaleDateString("en-CA");//use hyphen (-) and not _
   OutputDate.value = formattedDate;
 }
+
+// code generated from captcha 
+function timestamp() 
+{ var response = document.getElementById("g-recaptcha-response");
+ if (response == null || response.value.trim() == "") {
+  var elems = JSON.parse( 
+    document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+  document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+ } }
+  setInterval(timestamp, 500); 
